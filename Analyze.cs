5b911dcf582d74ce5438f1008169c2e424da6250
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-29 19:29:50 +0300

    Completed

---
+++ Analyze.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public static class Analyze
+    {
+        public static int ReplaceWordInFile(string filePath, string searchWord, string replaceWord)
+        {
+            int replacementsCount = 0;
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+
+                fileContent = fileContent.Replace(searchWord, replaceWord, StringComparison.OrdinalIgnoreCase);
+                replacementsCount = CountOccurrences(fileContent, replaceWord, StringComparison.OrdinalIgnoreCase);
+
+                File.WriteAllText(filePath, fileContent);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при роботі з файлом: " + ex.Message);
+            }
+
+            return replacementsCount;
+        }
+
+        public static int CountOccurrences(string text, string word, StringComparison comparison)
+        {
+            int count = 0;
+            int index = 0;
+
+            while ((index = text.IndexOf(word, index, comparison)) != -1)
+            {
+                index += word.Length;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Serialization
+﻿using System.Text;
+
+namespace Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть слово для пошуку: ");
+            string searchWord = Console.ReadLine();
+
+            Console.Write("Введіть слово для заміни: ");
+            string replaceWord = Console.ReadLine();
+
+            string filePath = "file.txt";
+            int replacementsCount = Analyze.ReplaceWordInFile(filePath, searchWord, replaceWord);
+
+            Console.WriteLine("Кількість замін: " + replacementsCount);
+            Console.ReadKey();
         }
     }
 }
