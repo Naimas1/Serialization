65435fd9e0f597d3754ba74743ea058a3cfc3858
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-29 19:45:58 +0300

    Completed

---
+++ Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public static class Numbers
+    {
+        public static void GenerateAndWriteNumbersToFile(string filePath, int count, int minValue, int maxValue)
+        {
+            try
+            {
+                Random random = new Random();
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        int randomNumber = random.Next(minValue, maxValue + 1);
+                        writer.WriteLine(randomNumber);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при записі у файл: " + ex.Message);
+            }
+        }
+        public static void SaveNumbersToFile(string filePath, List<int> numbers)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (int number in numbers)
+                    {
+                        writer.WriteLine(number);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при збереженні у файл: " + ex.Message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,71 @@
-﻿namespace Serialization
+﻿using System.Text;
+
+namespace Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string filePath = "numbers.txt";
+
+            int positiveCount = 0;
+            int negativeCount = 0;
+            int twoDigitCount = 0;
+            int fiveDigitCount = 0;
+
+            List<int> positiveNumbers = new List<int>();
+            List<int> negativeNumbers = new List<int>();
+            List<int> twoDigitNumbers = new List<int>();
+            List<int> fiveDigitNumbers = new List<int>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    if (int.TryParse(line, out int number))
+                    {
+                        if (number > 0)
+                        {
+                            positiveCount++;
+                            positiveNumbers.Add(number);
+                        }
+                        else if (number < 0)
+                        {
+                            negativeCount++;
+                            negativeNumbers.Add(number);
+                        }
+
+                        if (number >= 10 && number <= 99)
+                        {
+                            twoDigitCount++;
+                            twoDigitNumbers.Add(number);
+                        }
+                        else if (number >= 10000 && number <= 99999)
+                        {
+                            fiveDigitCount++;
+                            fiveDigitNumbers.Add(number);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Кількість додатніх чисел: " + positiveCount);
+                Console.WriteLine("Кількість від'ємних чисел: " + negativeCount);
+                Console.WriteLine("Кількість двозначних чисел: " + twoDigitCount);
+                Console.WriteLine("Кількість п'ятизначних чисел: " + fiveDigitCount);
+
+                Numbers.SaveNumbersToFile("positive_numbers.txt", positiveNumbers);
+                Numbers.SaveNumbersToFile("negative_numbers.txt", negativeNumbers);
+                Numbers.SaveNumbersToFile("two_digit_numbers.txt", twoDigitNumbers);
+                Numbers.SaveNumbersToFile("five_digit_numbers.txt", fiveDigitNumbers);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при роботі з файлом: " + ex.Message);
+            }
         }
     }
 }
