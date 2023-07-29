2ef00bd4663d620887653f6f5891739c151b31c6
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-29 19:35:12 +0300

    Completed

---
+++ Moderator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public static class Moderator
+    {
+        public static string[] ReadModerationWords(string filePath)
+        {
+            try
+            {
+                return File.ReadAllLines(filePath)
+                           .SelectMany(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                           .Distinct()
+                           .ToArray();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при зчитуванні файлу зі словами для модерації: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static string ModerateTextFile(string filePath, string[] moderationWords)
+        {
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+
+                foreach (string word in moderationWords)
+                {
+                    string replacement = new string('*', word.Length);
+                    fileContent = fileContent.Replace(word, replacement, StringComparison.OrdinalIgnoreCase);
+                }
+
+                return fileContent;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при роботі з файлом з текстом: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Serialization
+﻿using System.Text;
+
+namespace Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть шлях до файлу з текстом: ");
+            string textFilePath = Console.ReadLine();
+
+            Console.Write("Введіть шлях до файлу зі словами для модерації: ");
+            string moderationFilePath = Console.ReadLine();
+
+            string[] moderationWords = Moderator.ReadModerationWords(moderationFilePath);
+
+            if (moderationWords == null)
+            {
+                Console.WriteLine("Помилка при зчитуванні слів для модерації.");
+                Console.ReadKey();
+                return;
+            }
+
+            string resultText = Moderator.ModerateTextFile(textFilePath, moderationWords);
+
+            Console.WriteLine("Результат модерації:");
+            Console.WriteLine(resultText);
+
+            Console.ReadKey();
         }
     }
 }
