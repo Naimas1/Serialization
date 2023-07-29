0065fd888e57de2e865ff454b5d15e91f91fd3cf
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-29 19:40:50 +0300

    Completed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Serialization
+﻿using System.Text;
+
+namespace Serialization
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            string sourceFilePath = Console.ReadLine();
+
+            string reversedContent = Replace.ReverseFileContent(sourceFilePath);
+
+            if (!string.IsNullOrEmpty(reversedContent))
+            {
+                string destinationFilePath = "reversed_" + Path.GetFileName(sourceFilePath);
+                Replace.SaveToFile(destinationFilePath, reversedContent);
+                Console.WriteLine("Файл з перевернутим вмістом створено: " + destinationFilePath);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Replace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public static class Replace
+    {
+        public static string ReverseFileContent(string filePath)
+        {
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                char[] charArray = fileContent.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при роботі з файлом: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static void SaveToFile(string filePath, string content)
+        {
+            try
+            {
+                File.WriteAllText(filePath, content);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Помилка при збереженні у файл: " + ex.Message);
+            }
+        }
+    }
+}
